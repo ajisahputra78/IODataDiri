d92ee9cbe3a2e818b2d76e3d821d0db9f47370c4
Aji Sahputra <aji.saputro@studens.amikom.ac.id>
2020-09-11 21:31:37 +0700

    ‘initial’

--- IODataDiri/main.swift
+++ IODataDiri/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+//kode untuk memasukan data
+print("Masukan Nama Depan Anda : "); let firstName = readLine()!
+print("Masukan Nama Belakan Anda : "); let lastName = readLine()!
+print("Masukan Umur Anda : "); let age = readLine()!
+print("Masukan Alama Anda : "); let address = readLine()!
+print("Masukan Pekerjaan Anda : "); let job = readLine()!
 
+let fullName = firstName + "" + lastName
+
+
+print("----------------------------------------")
+
+//Ini adalah output program
+print("Apakah Kalian tahu \(fullName)?")
+print("\(firstName) Adalah seorang \(job)")
+print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address)")
+print("----------------------------------------")
